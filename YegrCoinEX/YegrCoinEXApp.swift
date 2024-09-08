e0a19f68183670039b01d67c1e354bca46ea625d
yeggrrr <yjkim981103@gmail.com>
2024-09-08 19:03:09 +0900

    Initial Commit

---
+++ YegrCoinEX/YegrCoinEXApp.swift
@@ -0,0 +1,17 @@
+//
+//  YegrCoinEXApp.swift
+//  YegrCoinEX
+//
+//  Created by YJ on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct YegrCoinEXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
